a7479d5a196919422fef547b0a6113e267c402a8
Julian Kornberger <jk+github@digineo.de>
2016-03-21 12:26:08 +0100

    Add test for nodes.Update()

--- models/nodes_test.go
+++ models/nodes_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/FreifunkBremen/respond-collector/data"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,6 +21,21 @@ func TestLoadAndSave(t *testing.T) {
 	tmpfile, _ := ioutil.TempFile("/tmp", "nodes")
 	save(nodes, tmpfile.Name())
 	os.Remove(tmpfile.Name())
+
+	assert.Equal(1, len(nodes.List))
+}
+
+func TestUpdateNodes(t *testing.T) {
+	assert := assert.New(t)
+	nodes := &Nodes{List: make(map[string]*Node)}
+	assert.Equal(0, len(nodes.List))
+
+	res := &data.ResponseData{
+		Neighbours: &data.Neighbours{},
+		Statistics: &data.Statistics{},
+		NodeInfo:   &data.NodeInfo{},
+	}
+	nodes.Update("abcdef012345", res)
 
 	assert.Equal(1, len(nodes.List))
 }
