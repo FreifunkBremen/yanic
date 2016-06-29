5011719fdf9ab4584c9d15376e2d940c2c143c52
Martin Geno <geno+dev@fireorbit.de>
2016-06-29 20:34:32 +0200

    optional channel and txpower

--- data/breminale.go
+++ data/breminale.go
@@ -1,10 +1,10 @@
 package data
 
 type Wireless struct {
-	TxPower24 uint32 `json:"txpower24"`
-	Channel24 uint32 `json:"channel24"`
-	TxPower5  uint32 `json:"txpower5"`
-	Channel5  uint32 `json:"channel5"`
+	TxPower24 uint32 `json:"txpower24,omitempty"`
+	Channel24 uint32 `json:"channel24,omitempty"`
+	TxPower5  uint32 `json:"txpower5,omitempty"`
+	Channel5  uint32 `json:"channel5,omitempty"`
 }
 type SwitchPort struct {
 	Speed uint32 `json:"speed"`
