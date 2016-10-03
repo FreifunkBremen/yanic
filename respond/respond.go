d2be43530810b10760a3a3748aedec279e21c667
Julian Kornberger <jk+github@digineo.de>
2016-10-04 01:08:07 +0200

    Omit data types

--- respond/respond.go
+++ respond/respond.go
@@ -6,13 +6,13 @@ import (
 
 const (
 	// default multicast group used by announced
-	multiCastGroup string = "ff02:0:0:0:0:0:2:1001"
+	multiCastGroup = "ff02:0:0:0:0:0:2:1001"
 
 	// default udp port used by announced
-	port string = "1001"
+	port = "1001"
 
 	// maximum receivable size
-	maxDataGramSize int = 8192
+	maxDataGramSize = 8192
 )
 
 //Response of the respond request
