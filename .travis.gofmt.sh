bd398847ba4647bb6a5002adcbb08f37dde73db2
Julian Kornberger <jk+github@digineo.de>
2018-05-13 21:07:48 +0200

    Ignore vendor/ for CI

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result="$(gofmt -s -l .)"
+result="$(gofmt -s -l . | grep -v '^vendor/' )"
 if [ -n "$result" ]; then
   echo "Go code is not formatted, run 'gofmt -s -w .'" >&2
   echo "$result"
