fcc09f49e85def62e3affc71fe6e98292abdef38
Krombel <krombel@krombel.de>
2019-05-16 04:02:09 +0200

    rewrite geojson output to use definitons of external lib

--- output/geojson/geojson.go
+++ output/geojson/geojson.go
@@ -1,59 +1,27 @@
 package geojson
 
 import (
-	"github.com/FreifunkBremen/yanic/lib/jsontime"
 	"github.com/FreifunkBremen/yanic/runtime"
+	"github.com/paulmach/go.geojson"
 )
 
-type GeoJSON struct {
-	Type      string        `json:"type"`
-	Timestamp jsontime.Time `json:"updated_at"` // Timestamp of the generation
-	Features  []*Feature    `json:"features"`
-}
+func transform(nodes *runtime.Nodes) *geojson.FeatureCollection {
+	nodelist := geojson.NewFeatureCollection()
 
-type Feature struct {
-	Type       string            `json:"type"`
-	Properties map[string]string `json:"properties"`
-	Geometry   Geometry          `json:"geometry"`
-}
-
-type Geometry struct {
-	Type        string    `json:"type"`
-	Coordinates []float64 `json:"coordinates"`
-}
-
-func NewPoint(n *runtime.Node) *Feature {
-	if n.Nodeinfo == nil || n.Nodeinfo.Location == nil {
-		return nil
-	}
-	location := n.Nodeinfo.Location
-
-	return &Feature{
-		Type: "Feature",
-		Properties: map[string]string{
-			"name": n.Nodeinfo.Hostname,
-		},
-		Geometry: Geometry{
-			Type: "Point",
-			Coordinates: []float64{
-				location.Longitude,
-				location.Latitude,
-			},
-		},
-	}
-}
-
-func transform(nodes *runtime.Nodes) *GeoJSON {
-	nodelist := &GeoJSON{
-		Type:      "FeatureCollection",
-		Timestamp: jsontime.Now(),
-	}
-
-	for _, nodeOrigin := range nodes.List {
-		point := NewPoint(nodeOrigin)
-		if point != nil {
-			nodelist.Features = append(nodelist.Features, point)
+	for _, n := range nodes.List {
+		if n.Nodeinfo == nil || n.Nodeinfo.Location == nil {
+			continue
 		}
+		nodeinfo := n.Nodeinfo
+		location := nodeinfo.Location
+		point := geojson.NewPointFeature([]float64{
+			location.Longitude,
+			location.Latitude,
+		})
+		point.Properties["id"] = nodeinfo.NodeID
+		point.Properties["name"] = nodeinfo.Hostname
+
+		nodelist.Features = append(nodelist.Features, point)
 	}
 	return nodelist
 }
