6d310614ca32609666ca619c29d3961af60732c0
nrbffs <38812627+nrbffs@users.noreply.github.com>
2019-11-13 12:14:15 +0100

    [TASK] add output raw (#169)

--- output/all/main.go
+++ output/all/main.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/FreifunkBremen/yanic/output/meshviewer"
 	_ "github.com/FreifunkBremen/yanic/output/meshviewer-ffrgb"
 	_ "github.com/FreifunkBremen/yanic/output/nodelist"
+	_ "github.com/FreifunkBremen/yanic/output/raw"
 )
