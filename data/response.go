3559c5359623fb878eb13dcb0c39753d1fc74eed
genofire <geno+dev@fireorbit.de>
2020-01-07 16:40:57 +0100

    gofmt

--- data/response.go
+++ data/response.go
@@ -2,8 +2,8 @@ package data
 
 // ResponseData struct
 type ResponseData struct {
-	Nodeinfo   *Nodeinfo   `json:"nodeinfo" toml:"nodeinfo"`
-	Statistics *Statistics `json:"statistics" toml:"statistics"`
-	Neighbours *Neighbours `json:"neighbours" toml:"neighbours"`
+	Nodeinfo     *Nodeinfo              `json:"nodeinfo" toml:"nodeinfo"`
+	Statistics   *Statistics            `json:"statistics" toml:"statistics"`
+	Neighbours   *Neighbours            `json:"neighbours" toml:"neighbours"`
 	CustomFields map[string]interface{} `json:"-"`
 }
