56b101e0f3d66ba61d28937e0e9b1e859b384ec2
David Bauer <mail@david-bauer.net>
2020-04-01 12:15:03 +0200

    database: graphite: add support for OWE statistics

--- database/graphite/global.go
+++ database/graphite/global.go
@@ -41,6 +41,9 @@ func GlobalStatsFields(name string, stats *runtime.GlobalStats) []graphigo.Metri
 		{Name: name + ".clients.wifi", Value: stats.ClientsWifi},
 		{Name: name + ".clients.wifi24", Value: stats.ClientsWifi24},
 		{Name: name + ".clients.wifi5", Value: stats.ClientsWifi5},
+		{Name: name + ".clients.owe", Value: stats.ClientsOwe},
+		{Name: name + ".clients.owe24", Value: stats.ClientsOwe24},
+		{Name: name + ".clients.owe5", Value: stats.ClientsOwe5},
 	}
 }
 
