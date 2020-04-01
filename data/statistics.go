4944790e065eaa0fa851c3c46128d15d292ab735
David Bauer <mail@david-bauer.net>
2020-04-01 12:12:58 +0200

    data: add support for OWE per-node statistics
    Gluon exports information about how many OWE capable clients are
    connected to a given node. The OWE client count is already contained in
    the wifi* counters and only provides more-specific information.

--- data/statistics.go
+++ data/statistics.go
@@ -63,6 +63,9 @@ type Clients struct {
 	Wifi   uint32 `json:"wifi"`
 	Wifi24 uint32 `json:"wifi24"`
 	Wifi5  uint32 `json:"wifi5"`
+	Owe    uint32 `json:"owe"`
+	Owe24  uint32 `json:"owe24"`
+	Owe5   uint32 `json:"owe5"`
 	Total  uint32 `json:"total"`
 }
 
